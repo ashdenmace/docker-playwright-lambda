6226f7a19cbda213966ef814127be5e08df6fa86
Ashden Mace <ashdenmace@gmail.com>
2025-07-08 17:57:10 -0700

    docker build current dir fix

--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
-cd "$(dirname "$0")"
 
-docker build -t playwright-lambda .
-mkdir -p ../dist
-docker run --rm -v "$PWD/../dist:/app" playwright-lambda
+docker build -f docker/Dockerfile -t playwright-lambda .
 
+mkdir -p dist
 
+docker run --rm -v "$PWD/dist:/app" playwright-lambda
 
 
 # chmod +x docker/build.sh
